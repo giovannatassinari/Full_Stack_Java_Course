cff928941228a474efbbf05c40cb7a151d6dc2ac
giovannatassinari <66632531+giovannatassinari@users.noreply.github.com>
2020-08-02 21:03:48 -0300

    Aula 7

---
+++ Aula 7 - JS/Exercícios/exercicios.js
@@ -0,0 +1,52 @@
+//Ex 1
+var x = 1;
+var y = -1;
+var z = 0.5;
+var nome = "maçã";
+var eMaior = 1 > 0.5;
+
+
+//Ex 2
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(nome);
+console.log(eMaior);
+
+//Ex 3
+var nome = "Giovanna";
+var sobrenome = "Tassinari";
+console.log(nome + " " + sobrenome);
+
+//Ex 4
+var nome = "Clara";
+console.log(Nome);
+//Dá erro, porque o nome da variável tem que ser escrito igual.
+
+//Ex 5
+var nome = "Clara";
+console.log(nome, sobrenome);
+//Dá erro, pois não existe a variável sobrenome.
+
+//Ex 6
+var texto = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " + 
+"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+"It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+"It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+console.log(texto);
+
+//Ex 7
+var valorNulo = null
+var naodefinido = undefined
+
+console.log(valorNulo);
+console.log(naodefinido);
+
+//Ex 8
+var array = ["maça", "laranja", "melão", "uva", "banana"];
+console.log(array);
+
+//Ex 9 
+var texto2 = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam."
+console.log(texto2);
